2ec96abc7329d040ef101bb4c09e2a461061e273
whitehawk <white-hawk-taka@outlook.com>
2015-04-07 14:09:21 +0900

    partition file

--- js/app.js
+++ js/app.js
@@ -1,317 +1,7 @@
 // oauthオブジェクト
 var oauth;
 
-var screenName = new Array();
-var name_str = new Array();
-var tweetText = new Array();
-var id_str = new Array();
-var prof_img_url = new Array();
-var favorited = new Array();
-
-var mention_screenName = new Array();
-var mention_name_str = new Array();
-var mention_tweetText = new Array();
-var mention_id_str = new Array();
-var mention_prof_img_url = new Array();
-var mention_favorited = new Array();
-
 localStorage.setItem("loading",0);
-
-
-
-window.onload = function(){
-	// OAuth関連の処理を開始する
-	var loadingop = localStorage.getItem("loading");
-	if(loadingop == 0){
-		firstOAuthFunc();
-	}
-
-};
-
-//タイムラインの削除
-var clearTweetDom = function(){
-	var parent = $("#tweetBox");
-	parent.empty();
-};
-
-//メンションの削除
-var clearMentionTweetDom = function(){
-	var parent = $("#mentionBox");
-	parent.empty();
-};
-
-//通常タイムライン取得OAuth
-var getHomeTimeline = function(){
-	var url = "https://api.twitter.com/1.1/statuses/home_timeline.json?count=100";
-	oauth.get(url, successGetHomeTimeline, failureTimeLineHandler);
-};
-
-//初回タイムライン取得OAuth
-var first_getHomeTimeline = function(){
-	var url = "https://api.twitter.com/1.1/statuses/home_timeline.json?count=100";
-	oauth.get(url, successFirstTimeline, failureTimeLineHandler);
-};
-
-//通常メンション取得OAuth
-var getMentionTimeline = function(){
-	var url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json?count=100";
-	oauth.get(url, successMention, failureTimeLineHandler);
-};
-
-//初回メンション取得OAuth
-var first_getMentionTimeline = function(){
-	var url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json?count=100";
-	oauth.get(url, successFirstMention, failureTimeLineHandler);
-};
-
-//初回タイムライン取得データ処理
-var successFirstTimeline = function(data){
-	clearTweetDom();
-	var tweetList = JSON.parse(data.text);
-	tweetIndex: for(var i = 0; i < tweetList.length; i++){
-		var tweet = tweetList[i];
-		for(var j = 0; j < id_str.length; j++){
-			var buf = id_str[j];
-			if(tweet.id_str == buf){
-				break tweetIndex;
-			}
-		}
-		screenName.unshift(tweet.user.screen_name);
-		name_str.unshift(tweet.user.name);
-		tweetText.unshift(tweet.text);
-		id_str.unshift(tweet.id_str);
-		prof_img_url.unshift(tweet.user.profile_image_url);
-		console.log("screenName:" + screenName);
-		console.log("name      :" + name_str);
-		console.log("tweetText :" + tweetText);
-		console.log("tweetID :" + id_str);
-
-	}
-	addFirstTweetToDom(tweet);
-};
-
-//初回タイムライン書き出し処理
-var addFirstTweetToDom = function(tweet){
-	for(var i = id_str.length - 1; i > 0; i--){
-		var buf_screenName = screenName[i];
-		var buf_name = name_str[i];
-		var buf_tweetText = tweetText[i];
-		var buf_prof_img_url = prof_img_url[i];
-		var buf_id_str = id_str[i];
-
-		var $parent = $("#tweetBox");
-		var $li = $("<li>").appendTo($parent);
-		var $div = $("<div>").addClass("tweet").appendTo($li);
-		var $userDiv = $("<div>").appendTo($div);
-		var $a1 = $("<a>").attr('href', "#drawer").appendTo($userDiv);
-
-		$("<img>").addClass("tweetIcon").attr('id', buf_id_str).attr('src', buf_prof_img_url).appendTo($userDiv);
-		$("<span>").addClass("name").text(buf_name).appendTo($userDiv);
-		$("<span>").addClass("screenName").text("@" + buf_screenName).appendTo($userDiv);
-		$("<img>").addClass("menu-button").attr('id', buf_id_str).attr('data-name', buf_screenName).attr('src', "img/icons/menu-button.png").appendTo($a1);
-		$("<div>").addClass("tweetText").text(buf_tweetText).appendTo($div);
-	}
-	$(".tweetText").each(function(){
-		$(this).html($(this).html().replace(/(https?|ftps?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g, '<a target="_blank" href="$&">$&</a>'));
-	});
-
-};
-
-//通常ライムライン取得データ処理
-var successGetHomeTimeline = function(data){
-	clearTweetDom();
-	var tweetList = JSON.parse(data.text);
-	var buf_screenName = new Array();
-	var buf_name_str = new Array();
-	var buf_tweetText = new Array();
-	var buf_id_str = new Array();
-	var buf_prof_img_url = new Array();
-
-	tweetIndex1: for(var i = 0; i < tweetList.length; i++){
-		var tweet = tweetList[i];
-		for(var j = 0; j < id_str.length; j++){
-			var buf = id_str[j];
-			if(tweet.id_str == buf){
-				break tweetIndex1;
-			}
-		}
-		buf_screenName.push(tweet.user.screen_name);
-		buf_name_str.push(tweet.user.name);
-		buf_tweetText.push(tweet.text);
-		buf_id_str.push(tweet.id_str);
-		buf_prof_img_url.push(tweet.user.profile_image_url);
-		console.log("screenName:" + screenName);
-		console.log("name      :" + name_str);
-		console.log("tweetText :" + tweetText);
-		console.log("tweetID :" + id_str);
-
-	}
-	buf_screenName.reverse();
-	buf_name_str.reverse();
-	buf_tweetText.reverse();
-	buf_id_str.reverse();
-	buf_prof_img_url.reverse();
-
-	for(var i = 0; i < buf_id_str.length; i++){
-		screenName.push(buf_screenName[i]);
-		name_str.push(buf_name_str[i]);
-		tweetText.push(buf_tweetText[i]);
-		id_str.push(buf_id_str[i]);
-		prof_img_url.push(buf_prof_img_url[i]);
-	}
-	addTweetToDom(tweet);
-};
-
-//通常タイムライン書き出し処理
-var addTweetToDom = function(tweet){
-	for(var i = id_str.length - 1; i > 0; i--){
-		var buf_screenName = screenName[i];
-		var buf_name = name_str[i];
-		var buf_tweetText = tweetText[i];
-		var buf_prof_img_url = prof_img_url[i];
-		var buf_id_str = id_str[i];
-
-		var $parent = $("#tweetBox");
-		var $li = $("<li>").appendTo($parent);
-		var $div = $("<div>").addClass("tweet").appendTo($li);
-		var $userDiv = $("<div>").appendTo($div);
-		var $a1 = $("<a>").attr('href', "#drawer").appendTo($userDiv);
-
-		$("<img>").addClass("tweetIcon").attr('id', buf_id_str).attr('src', buf_prof_img_url).appendTo($userDiv);
-		$("<span>").addClass("name").text(buf_name).appendTo($userDiv);
-		$("<span>").addClass("screenName").text("@" + buf_screenName).appendTo($userDiv);
-		$("<img>").addClass("menu-button").attr('id', buf_id_str).attr('data-name', buf_screenName).attr('src', "img/icons/menu-button.png").appendTo($a1);
-		$("<div>").addClass("tweetText").text(buf_tweetText).appendTo($div);
-	}
-	$(".tweetText").each(function(){
-		$(this).html($(this).html().replace(/(https?|ftps?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g, '<a target="_blank" href="$&">$&</a>'));
-	});
-
-};
-
-//初回メンション取得処理
-var successFirstMention = function(data){
-	clearMentionTweetDom();
-	var tweetList = JSON.parse(data.text);
-	mentionIndex: for(var i = 0; i < tweetList.length; i++){
-		var tweet = tweetList[i];
-		for(var j = 0; j < mention_id_str.length; j++){
-			var buf = mention_id_str[j];
-			if(tweet.id_str == buf){
-				break mentionIndex;
-			}
-		}
-		mention_screenName.unshift(tweet.user.screen_name);
-		mention_name_str.unshift(tweet.user.name);
-		mention_tweetText.unshift(tweet.text);
-		mention_id_str.unshift(tweet.id_str);
-		mention_prof_img_url.unshift(tweet.user.profile_image_url);
-		console.log("screenName:" + mention_screenName);
-		console.log("name      :" + mention_name_str);
-		console.log("tweetText :" + mention_tweetText);
-		console.log("tweetID :" + mention_id_str);
-
-	}
-	addFirstMentionTweetToDom(tweet);
-};
-
-//初回メンション書き出し処理
-var addFirstMentionTweetToDom = function(tweet){
-	for(var i = mention_id_str.length - 1; i > 0; i--){
-		var buf_screenName = mention_screenName[i];
-		var buf_name = mention_name_str[i];
-		var buf_tweetText = mention_tweetText[i];
-		var buf_prof_img_url = mention_prof_img_url[i];
-		var buf_id_str = mention_id_str[i];
-
-		var $parent = $("#mentionBox");
-		var $li = $("<li>").appendTo($parent);
-		var $div = $("<div>").addClass("tweet").appendTo($li);
-		var $userDiv = $("<div>").appendTo($div);
-		var $a1 = $("<a>").attr('href', "#drawer").appendTo($userDiv);
-
-		$("<img>").addClass("tweetIcon").attr('id', buf_id_str).attr('src', buf_prof_img_url).appendTo($userDiv);
-		$("<span>").addClass("name").text(buf_name).appendTo($userDiv);
-		$("<span>").addClass("screenName").text("@" + buf_screenName).appendTo($userDiv);
-		$("<img>").addClass("menu-button").attr('id', buf_id_str).attr('data-name', buf_screenName).attr('src', "img/icons/menu-button.png").appendTo($a1);
-		$("<div>").addClass("tweetText").text(buf_tweetText).appendTo($div);
-	}
-	$(".tweetText").each(function(){
-		$(this).html($(this).html().replace(/(https?|ftps?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g, '<a target="_blank" href="$&">$&</a>'));
-	});
-
-};
-
-//通常メンション取得処理
-var successMention = function(data){
-	clearMentionTweetDom();
-	var tweetList = JSON.parse(data.text);
-	var buf_screenName = new Array();
-	var buf_name_str = new Array();
-	var buf_tweetText = new Array();
-	var buf_id_str = new Array();
-	var buf_prof_img_url = new Array();
-	mentionIndex1: for(var i = 0; i < tweetList.length; i++){
-		var tweet = tweetList[i];
-		for(var j = 0; j < id_str.length; j++){
-			var buf = id_str[j];
-			if(tweet.id_str == buf){
-				break mentionIndex1;
-			}
-		}
-		buf_screenName.push(tweet.user.screen_name);
-		buf_name_str.push(tweet.user.name);
-		buf_tweetText.push(tweet.text);
-		buf_id_str.push(tweet.id_str);
-		buf_prof_img_url.push(tweet.user.profile_image_url);
-		console.log("screenName:" + mention_screenName);
-		console.log("name      :" + mention_name_str);
-		console.log("tweetText :" + mention_tweetText);
-		console.log("tweetID :" + mention_id_str);
-
-	}
-	buf_screenName.reverse();
-	buf_name_str.reverse();
-	buf_tweetText.reverse();
-	buf_id_str.reverse();
-	buf_prof_img_url.reverse();
-
-	for(var i = 0; i < buf_id_str.length; i++){
-		mention_screenName.push(buf_screenName[i]);
-		mention_name_str.push(buf_name_str[i]);
-		mention_tweetText.push(buf_tweetText[i]);
-		mention_id_str.push(buf_id_str[i]);
-		mention_prof_img_url.push(buf_prof_img_url[i]);
-	}
-	addMentionTweetToDom(tweet);
-};
-
-//通常メンション書き出し処理
-var addMentionTweetToDom = function(tweet){
-	for(var i = mention_id_str.length - 1; i > 0; i--){
-		var buf_screenName = mention_screenName[i];
-		var buf_name = mention_name_str[i];
-		var buf_tweetText = mention_tweetText[i];
-		var buf_prof_img_url = mention_prof_img_url[i];
-		var buf_id_str = mention_id_str[i];
-
-		var $parent = $("#mentionBox");
-		var $li = $("<li>").appendTo($parent);
-		var $div = $("<div>").addClass("tweet").appendTo($li);
-		var $userDiv = $("<div>").appendTo($div);
-		var $a1 = $("<a>").attr('href', "#drawer").appendTo($userDiv);
-
-		$("<img>").addClass("tweetIcon").attr('id', buf_id_str).attr('src', buf_prof_img_url).appendTo($userDiv);
-		$("<span>").addClass("name").text(buf_name).appendTo($userDiv);
-		$("<span>").addClass("screenName").text("@" + buf_screenName).appendTo($userDiv);
-		$("<img>").addClass("menu-button").attr('id', buf_id_str).attr('data-name', buf_screenName).attr('src', "img/icons/menu-button.png").appendTo($a1);
-		$("<div>").addClass("tweetText").text(buf_tweetText).appendTo($div);
-	}
-	$(".tweetText").each(function(){
-		$(this).html($(this).html().replace(/(https?|ftps?)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/g, '<a target="_blank" href="$&">$&</a>'));
-	});
-
-};
 
 //ツイート
 var newTweetPost = function(){
@@ -337,7 +27,7 @@ var newTweetPost = function(){
 };
 
 //リプツイ
-var replyTweetPost = function(){
+var replyTweetPost = function(repId){
 	var data;
 	var statusText = document.getElementById("newTweetText").value;
 	var file = document.querySelector("#file").files[0];
@@ -393,8 +83,6 @@ var firstOAuthFunc = function(){
 		localStorage.setItem("firstoauth", 1);
 		if(accessTokenKey){
 			oauth.setAccessToken(accessTokenKey, accessTokenSecret);
-			first_getHomeTimeline();
-			first_getMentionTimeline();
 		}else{
 			// 1. consumer key と consumer secret を使って、リクエストトークンを取得する
 			oauth.fetchRequestToken(successFetchRequestToken, failureHandler);
@@ -417,7 +105,7 @@ var firstOAuthFunc = function(){
 
 // 1の処理の成功時のコールバック関数
 var successFetchRequestToken = function (authUrl) {
-
+	localStorage.setItem("firstoauth", 0);
 	//URLを訂正
 	var tmp = authUrl.indexOf("&");
 	var authUrl2 = authUrl.substring(0,tmp);
@@ -432,18 +120,18 @@ var successFetchRequestToken = function (authUrl) {
 
 	// oauthオブジェクトにPINをセット
 	oauth.setVerifier(pin);
-
+	localStorage.setItem("firstoauth", 1);
 	// 5. consumer key, consumer secret, リクエストトークン, PIN を使って、アクセストークンを取得する
 	oauth.fetchAccessToken(successFetchAccessToken, failureHandler);
 };
 
 // 5の処理の成功時のコールバック関数
 var successFetchAccessToken = function () {
+	localStorage.setItem("firstoauth", 0);
 	localStorage.setItem("accessTokenKey", oauth.getAccessTokenKey());
 	localStorage.setItem("accessTokenSecret", oauth.getAccessTokenSecret());
 	localStorage.setItem("firstoauth",1);
 	alert("success oauth");
-	getHomeTimeline();
 
 };
 
@@ -464,6 +152,10 @@ var failurePostHandler = function(data){
 
 var nonerror = function(data){
 };
+
+var faulureGetUser = function(data){
+	alert("ユーザー情報の取得に失敗しました");
+}
 
 /***ふぁぼ***/
 var favoriteCreate = function(favId){
