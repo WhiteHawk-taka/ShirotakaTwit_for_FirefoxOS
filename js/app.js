7d9afc55ddd86e918cb22aecd378a2d51fb89336
whitehawk <white-hawk-taka@outlook.com>
2015-07-21 17:25:08 +0900

    Improvement of visibility

--- js/app.js
+++ js/app.js
@@ -28,15 +28,109 @@ window.onload = function(){
 
 };
 
+
+// OAuth処理 ///////////////////////////////////////////////////////////////////////////////
+var firstOAuthFunc = function(){
+	var firstoauth = localStorage.getItem("firstoauth");
+
+	if(firstoauth == 1){
+		localStorage.setItem("firstoauth",0);
+		// 最初にOAuthオブジェクトに喰わせる値たち
+		var ck = ckload();
+		var cs = csload();
+		var config = {
+			consumerKey:ck,
+			consumerSecret:cs,
+			requestTokenUrl:"https://api.twitter.com/oauth/request_token",
+			authorizationUrl:"https://api.twitter.com/oauth/authorize",
+			accessTokenUrl:"https://api.twitter.com/oauth/access_token"
+		};
+
+		// OAuthのオブジェクトを作成
+		oauth = new OAuth(config);
+
+		//保存してあるアクセストークンがあればロードする
+		var accessTokenKey = localStorage.getItem("accessTokenKey");
+		var accessTokenSecret = localStorage.getItem("accessTokenSecret");
+		localStorage.setItem("firstoauth", 1);
+		if(accessTokenKey){
+			oauth.setAccessToken(accessTokenKey, accessTokenSecret);
+			first_getHomeTimeline();
+			first_getMentionTimeline();
+		}else{
+			// 1. consumer key と consumer secret を使って、リクエストトークンを取得する
+			oauth.fetchRequestToken(successFetchRequestToken, failureHandler);
+		}
+
+	}else{
+
+		if(window.confirm('Twitterで認証を行ってください')){
+			localStorage.setItem("firstoauth", 1);
+
+			firstOAuthFunc();
+		}else{
+			window.alert('中止されました');
+			localStorage.setTimeout("firstoauth", 0);
+			return;
+		}
+	}
+
+};
+
+// 1の処理の成功時のコールバック関数
+var successFetchRequestToken = function (authUrl) {
+
+	//URLを訂正
+	var tmp = authUrl.indexOf("&");
+	var authUrl2 = authUrl.substring(0,tmp);
+
+
+	// 2. リクエストトークンを使い、ユーザにアクセス許可を求めるURLを生成して ブラウザを起動
+	// 3. ブラウザで認証を行い、ユーザーにPINが表示される
+	//window.open(authUrl2);
+	new MozActivity({
+    	"name":"view",
+    	"data":
+      	{
+        	"type":"url",
+        	"url":authUrl2
+    	}
+	});
+
+	// 4. アプリで用意したダイアログにPIN を入力してもらう
+	var pin = prompt("Please enter your PIN", "");
+
+	// oauthオブジェクトにPINをセット
+	oauth.setVerifier(pin);
+
+	// 5. consumer key, consumer secret, リクエストトークン, PIN を使って、アクセストークンを取得する
+	oauth.fetchAccessToken(successFetchAccessToken, failureHandler);
+};
+
+// 5の処理の成功時のコールバック関数
+var successFetchAccessToken = function () {
+	localStorage.setItem("firstoauth", 0);
+	localStorage.setItem("accessTokenKey", oauth.getAccessTokenKey());
+	localStorage.setItem("accessTokenSecret", oauth.getAccessTokenSecret());
+	localStorage.setItem("firstoauth",1);
+	alert("success oauth");
+	getHomeTimeline();
+
+};
+
+
+// 各処理失敗時のコールバック関数
+var failureHandler = function (data) {
+	localStorage.setItem("firstoauth",0);
+	alert("failure");
+};
+
+
+
+//タイムラインに関する処理/////////////////////////////////////////////////////////////////
 //タイムラインの削除
 var clearTweetDom = function(){
 	var parent = $("#tweetBox");
-	parent.empty();
-};
-
-//メンションの削除
-var clearMentionTweetDom = function(){
-	var parent = $("#mentionBox");
 	parent.empty();
 };
 
@@ -189,6 +283,14 @@ var addTweetToDom = function(tweet){
 
 };
 
+
+//メンションに関する処理//////////////////////////////////////////////////////////
+//メンションの削除
+var clearMentionTweetDom = function(){
+	var parent = $("#mentionBox");
+	parent.empty();
+};
+
 //初回メンション取得処理
 var successFirstMention = function(data){
 	clearMentionTweetDom();
@@ -313,6 +415,8 @@ var addMentionTweetToDom = function(tweet){
 
 };
 
+
+//ポスト系の処理///////////////////////////////////////////////////////////
 //ツイート
 var newTweetPost = function(){
 	var data;
@@ -359,121 +463,6 @@ var replyTweetPost = function(repId){
 	}
 };
 
-//成功時の出力ログ
-var successHandler = function(){
-	console.log("success");
-};
-
-
-
-
-// OAuth関連で最初に行う処理
-var firstOAuthFunc = function(){
-	var firstoauth = localStorage.getItem("firstoauth");
-
-	if(firstoauth == 1){
-		localStorage.setItem("firstoauth",0);
-		// 最初にOAuthオブジェクトに喰わせる値たち
-		var ck = ckload();
-		var cs = csload();
-		var config = {
-			consumerKey:ck,
-			consumerSecret:cs,
-			requestTokenUrl:"https://api.twitter.com/oauth/request_token",
-			authorizationUrl:"https://api.twitter.com/oauth/authorize",
-			accessTokenUrl:"https://api.twitter.com/oauth/access_token"
-		};
-
-		// OAuthのオブジェクトを作成
-		oauth = new OAuth(config);
-
-		//保存してあるアクセストークンがあればロードする
-		var accessTokenKey = localStorage.getItem("accessTokenKey");
-		var accessTokenSecret = localStorage.getItem("accessTokenSecret");
-		localStorage.setItem("firstoauth", 1);
-		if(accessTokenKey){
-			oauth.setAccessToken(accessTokenKey, accessTokenSecret);
-			first_getHomeTimeline();
-			first_getMentionTimeline();
-		}else{
-			// 1. consumer key と consumer secret を使って、リクエストトークンを取得する
-			oauth.fetchRequestToken(successFetchRequestToken, failureHandler);
-		}
-
-	}else{
-
-		if(window.confirm('Twitterで認証を行ってください')){
-			localStorage.setItem("firstoauth", 1);
-
-			firstOAuthFunc();
-		}else{
-			window.alert('中止されました');
-			localStorage.setTimeout("firstoauth", 0);
-			return;
-		}
-	}
-
-};
-
-// 1の処理の成功時のコールバック関数
-var successFetchRequestToken = function (authUrl) {
-
-	//URLを訂正
-	var tmp = authUrl.indexOf("&");
-	var authUrl2 = authUrl.substring(0,tmp);
-
-
-	// 2. リクエストトークンを使い、ユーザにアクセス許可を求めるURLを生成して ブラウザを起動
-	// 3. ブラウザで認証を行い、ユーザーにPINが表示される
-	//window.open(authUrl2);
-	new MozActivity({
-    	"name":"view",
-    	"data":
-      	{
-        	"type":"url",
-        	"url":authUrl2
-    	}
-	});
-
-	// 4. アプリで用意したダイアログにPIN を入力してもらう
-	var pin = prompt("Please enter your PIN", "");
-
-	// oauthオブジェクトにPINをセット
-	oauth.setVerifier(pin);
-
-	// 5. consumer key, consumer secret, リクエストトークン, PIN を使って、アクセストークンを取得する
-	oauth.fetchAccessToken(successFetchAccessToken, failureHandler);
-};
-
-// 5の処理の成功時のコールバック関数
-var successFetchAccessToken = function () {
-	localStorage.setItem("firstoauth", 0);
-	localStorage.setItem("accessTokenKey", oauth.getAccessTokenKey());
-	localStorage.setItem("accessTokenSecret", oauth.getAccessTokenSecret());
-	localStorage.setItem("firstoauth",1);
-	alert("success oauth");
-	getHomeTimeline();
-
-};
-
-
-// 各処理失敗時のコールバック関数
-var failureHandler = function (data) {
-	localStorage.setItem("firstoauth",0);
-	alert("failure");
-};
-
-var failureTimeLineHandler = function(data){
-	alert("タイムラインの取得に失敗しました");
-};
-
-var failurePostHandler = function(data){
-	alert("ツイートに失敗しました");
-};
-
-var nonerror = function(data){
-};
-
 /***ふぁぼ***/
 var favoriteCreate = function(favId){
 	oauth.request({
@@ -498,5 +487,29 @@ var rtCreate = function(rtId){
 		url:"https://api.twitter.com/1.1/statuses/retweet/" + rtId + ".json",
 	});
 };
+
+
+
+
+//エラー系のダイアログ処理/////////////////////////////////////////////////////
+//成功時の出力ログ
+var successHandler = function(){
+	console.log("success");
+};
+
+
+var failureTimeLineHandler = function(data){
+	alert("タイムラインの取得に失敗しました");
+};
+
+var failurePostHandler = function(data){
+	alert("ツイートに失敗しました");
+};
+
+var nonerror = function(data){
+};
+
+
+
 
 
